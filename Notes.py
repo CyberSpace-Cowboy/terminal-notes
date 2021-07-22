51aa4777ac8add6d2093c8e1ae94c1020a641eaa
Ivan Tellurian <ivantellurian@Ivans-MacBook-Air.local>
2021-07-22 16:50:46 +0300

    new feature - reading notes

--- Notes.py
+++ Notes.py
@@ -1,5 +1,11 @@
 #Python Terminal Notes
 
+#for outputing only text files
+import glob
+#for centering output
+import shutil
+columns = shutil.get_terminal_size().columns
+
 def Write_Notes():
 	
 	#Naming a note
@@ -22,22 +28,66 @@ def Write_Notes():
 	#Closing the file
 	file.close()
 	
-
-
-	#Wanna see your note?
+	#Asking if a user wants to see his note?
 	ask = input("\n Wanna see your note? (y/n): ")
 	if ask == "y":
 		print("\n Here is Your Note: \n")
 	elif ask == "n":
-		exit()
-
+		return
+	#Reading a note
+	print("\n" + f_name.upper().center(columns) + "\n")
 	file = open(f_name + '.txt', "r")
 	print(file.read())
-	
+
+def Read_Notes():
+	#to check if there's no .txt files
+	if [] == glob.glob("*.txt"):
+		print("Sorry, there are no notes to read, we suggest you to take a note first")
+		return
+	else:
+	#to get the file name to output
+		print("\n Please, choose which notes you want to read: \n")
+		i = 1
+		for note in glob.glob("*.txt"):
+			print(str(i) + " - " + note.split('.txt')[0])
+			i += 1
+
+		#to input a number of iterations
+		while True:
+			try:
+				num = int(input("\n Type the number of the note: "))
+				print("\n"+ glob.glob("*.txt")[num-1].split('.txt')[0].upper().center(columns) + "\n")
+				break
+			except ValueError:
+				print("\n Please input integer only...")
+				continue
+				
+	#grabbing the file name to read it
+	f_name = glob.glob("*.txt")[num-1]		
+	file = open(f_name, "r")
+	print(file.read())
+
+def Choose_Action():
+	print("\n" + "TERMINAL NOTES".center(columns) + "\n")
+
+
+	while(True):
+		mode = input("\n Choose between Write mode and Read mode (W/R): ")
+		if mode.lower() == "w":
+			Write_Notes()
+		if mode.lower() == "r":
+			Read_Notes()
+
+		ask = input("\n Wanna Continue? (y/n): ")
+		if ask.lower() == "y":
+			continue
+		elif ask.lower() == "n":
+			exit()
+
 
 if __name__ == '__main__':
       
     # main method for executing
     # the functions
-    Write_Notes()
+    Choose_Action()
 
